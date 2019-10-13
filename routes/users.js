cd9f0637b72263121b598d967814014deb15b003
Hunny123 <hunny@inviz.ai>
2019-10-13 19:55:39 +0530

    [Hunny] updated the upload api

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
+const filevalidate = (file) => {
+
+  var ext = file.originalname.split(".").pop();
+  if (ext !== 'png' && ext !== 'jpg' && ext !== 'gif' && ext !== 'jpeg') {
+
+    return false
+  }
+  return true
+};
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt-nodejs");
@@ -5,6 +14,67 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const config = require("../config/secret");
 const User = require("../models/user");
+const { Storage } = require('@google-cloud/storage');
+const multer = require('multer');
+
+const storage = new Storage({
+  projectId: "capstone-5e12b",
+  keyFilename: "./config/firebase.json"
+});
+const bucket = storage.bucket("capstone-5e12b.appspot.com");
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+  },
+  fileFilter: function (req, file, callback) {
+    var ext = file.originalname.split(".").pop();
+    if (ext !== 'png' && ext !== 'jpg' && ext !== 'gif' && ext !== 'jpeg') {
+     callback(null,false)
+    }
+    callback(null, true)
+  },
+ 
+});
+ 
+// @param { File } file 
+ async function uploadImageToStorage(file){
+   return await new Promise((resolve, reject) => {
+    console.log("call")
+    if (!file) {
+      reject('No image file');
+    }
+    let newFileName = `${file.originalname}_${Date.now()}`;
+
+    let fileUpload = bucket.file(newFileName);
+    
+   
+    
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+
+    blobStream.on('error', (error) => {
+      reject('Something is wrong! Unable to upload at the moment.'+error);
+    });
+
+    blobStream.on('finish', () => {
+      
+      // The public URL can be used to directly access the file via HTTP.
+      const url = `https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`;
+      fileUpload.makePublic(function (err, apiResponse) {
+        
+      })
+      resolve(url);
+    });
+
+    blobStream.end(file.buffer);
+  });
+}
+
 
 router.post("/register", (req, res) => {
   console.log(req.body);
@@ -110,36 +180,73 @@ router.get(
     res.json({ user: req.user });
   }
 );
-router.post(
-  "/update",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    if (req.body.userDetails.contactNumber && req.body.userDetails.contactNumber.length!==10 ) {
-      return res.status(400).json({ success: false, msg: "enter 10 digit or leave it blank" })
+
+router.post('/upload', passport.authenticate("jwt", { session: false }), upload.single('file'), (req, res) => {
+  console.log("i used")
+  let file = req.file;
+  const user = JSON.parse(req.body.user)
+  console.log(req)
+  if (file) {
+    if (!filevalidate) {
+      return res.status(400).json({ "mess": "please image only" })
     }
-    
-   
-    // use user model to find the user 
-    User.findByIdAndUpdate(req.body.id ,
-      {
+
+    else {
+      uploadImageToStorage(file).then((success) => {
+        user.userDetails.profileImg = success
        
-        userDetails:req.body.userDetails
-        
-      }, function (err, doc) {
+           if (user.userDetails.contactNumber && user.userDetails.contactNumber.length !== 10) {
+              return res.status(400).json({ success: false, msg: "enter 10 digit or leave it blank" })
+            }
+  User.findByIdAndUpdate(user.id,
+    {
 
-        if (err) {
-        
-          return res.status(400).json({ success: false, msg: "something thing wrong" })
+      userDetails: user.userDetails
 
-        } else {
-          
-         return res.json({ message: 'user updated!'})
+    }, function (err, doc) {
 
-        }
+      if (err) {
 
+        return res.status(400).json({ success: false, msg: "something thing wrong" })
+
+      } else {
+
+        return res.json({ message: 'user updated !' })
+
+      }
+
+    })
+       
+      }).catch((error) => {
+        return res.status(400).json({ "success": false, msg: error })
       });
-
+    }
   }
-);
+  else {
+  
+  if (user.userDetails.contactNumber && user.userDetails.contactNumber.length !== 10) {
+    return res.status(400).json({ success: false, msg: "enter 10 digit or leave it blank" })
+  }
+  User.findByIdAndUpdate(user.id,
+    {
+
+      userDetails: user.userDetails
+
+    }, function (err, doc) {
+
+      if (err) {
+
+        return res.status(400).json({ success: false, msg: "something thing wrong" })
+
+      } else {
+
+        return res.json({ message: 'user updated !' })
+
+      }
+
+    })
+  }
+});
+
 
 module.exports = router;
